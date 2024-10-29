e14fc141e50b143f3bd80015d09e26d6954a1df3
rhenwinch <rhenwinchester@gmail.com>
2024-10-29 16:23:01 +0800

    feat: add all placeholder files for planned sections

--- theme.config.tsx
+++ theme.config.tsx
@@ -74,16 +74,16 @@ export default defineTheme({
                 group('Provider', [
                     link('Overview', '/provider/overview'),
                     link('Configuration', '/provider/configuration'),
-                    link('Creating a provider', '/provider/creating_a_provider'),
+                    link('Creating a provider', '/provider/create_provider'),
+                    link('Testing a provider', '/provider/test_provider'),
                 ]),
-                group('Examples', [
-                    link('Implementing settings', '/examples/impl_settings'),
-                    link('Loading catalogs', '/examples/loading_catalogs'),
-                    link('Fetching metadata', '/examples/fetching_metadata'),
-                    link('Searching media', '/examples/searching_media'),
-                    link('Implementing filters', '/examples/impl_filters'),
-                    link('Loading links', '/examples/get_links'),
-                    link('Testing', '/examples/get_links'),
+                group('Implementation', [
+                    link('Settings UI', '/impl/impl_settings'),
+                    link('Loading catalogs', '/impl/loading_catalogs'),
+                    link('Fetching metadata', '/impl/fetching_metadata'),
+                    link('Searching media', '/impl/searching_media'),
+                    link('Search filters', '/impl/impl_filters'),
+                    link('Fetching media links', '/impl/fetching_links'),
                 ]),
                 group('Advanced', [
                     link('WebView', '/advanced/webview'),
